2c699c1bbc8d193676fd3cc40cb40204e88b0c8c
Hanyu Duan <HanyuDuan@outlook.com>
2024-04-23 17:38:35 +0800

    rewrite decorators

--- api/decorators.py
+++ api/decorators.py
@@ -1,6 +1,34 @@
-from django.utils.decorators import decorator_from_middleware
+# the official decorator_from_middleware, decorator_from_middleware_with_args
+# isn't working. don't know why
 
-from .middleware import LoginRequiredMiddleware, AllowMethodsMiddleware
+from http import HTTPStatus
 
-login_required = decorator_from_middleware(LoginRequiredMiddleware)
-allow_methods = decorator_from_middleware(AllowMethodsMiddleware)
+from django.http import JsonResponse
+
+
+def allow_methods(allowed_methods: list[str]):
+    '''
+    the decorated function must be called with certain http methods
+    '''
+    def decor(func):
+        def wrapper(request):
+            print(type(request))
+            if request.method not in allowed_methods:
+                return JsonResponse({'error': 'Method Not Allowed'}, status=HTTPStatus.METHOD_NOT_ALLOWED)
+            return func(request)
+        return wrapper
+    return decor
+
+
+def login_required():
+    '''
+    decorated function must be called after user is authenticated
+    '''
+    def decor(func):
+        def wrapper(request):
+            if not request.user.is_authenticated:
+                # or not request.user.is_permitted:
+                return JsonResponse({'error': 'Unauthorized action, login required'}, status=HTTPStatus.UNAUTHORIZED)
+            return func(request)
+        return wrapper
+    return decor
