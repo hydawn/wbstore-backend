2d2b64a821c82e1440b36a13f3204c378696c3a3
Hanyu Duan <HanyuDuan@outlook.com>
2024-04-24 09:29:07 +0800

    api does not end with slash

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from .views import CsrfTokenAPI, LoginAPI, SignupAPI, get_user_detail, get_echo,
         get_user_loggedin
 
 urlpatterns = [
-    path('login/', LoginAPI.as_view()),
-    path('signup/', SignupAPI.as_view()),
-    path('get_csrf_token/', CsrfTokenAPI.as_view()),
-    path('get_user_detail/', get_user_detail),
+    path('login', LoginAPI.as_view()),
+    path('signup', SignupAPI.as_view()),
+    path('get_csrf_token', CsrfTokenAPI.as_view()),
+    path('get_user_detail', get_user_detail),
     path('get_user_loggedin', get_user_loggedin),
     path('echo', get_echo),
 ]
